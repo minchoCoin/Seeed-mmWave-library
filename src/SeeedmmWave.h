6b9a9523cb8651f056a8fba0d759cbb649e6770a
Spencer Yan <spencer@spenyan.com>
2024-07-18 17:07:22 +0800

    refactor: :ambulance: cannot get complete packets

--- src/SeeedmmWave.h
+++ src/SeeedmmWave.h
@@ -4,6 +4,20 @@
 #include <Arduino.h>
 #include <stdint.h>
 
+#ifdef ESP_ARDUINO_VERSION_MAJOR
+#  if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+// Code for version 3.x
+#  else
+// Code for version 2.x
+#  endif
+#else
+// Code for version 1.x
+#endif
+
+#ifndef ESP32
+#  error "Currently this library only supports ESP32"
+#endif
+
 #include <memory>
 #include <queue>
 
@@ -29,19 +43,40 @@
   (SIZE_SOF + SIZE_ID + SIZE_LEN + SIZE_TYPE + SIZE_HEAD_CKSUM)
 #define SIZE_DATA_CKSUM 1
 
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#  define SEEED_WAVE_IS_BIG_ENDIAN 1
+#elif defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#  define SEEED_WAVE_IS_BIG_ENDIAN 0
+#else
+#  warning "Unable to determine the size end system"
+#endif
+
+#ifndef ESP32
+
+#endif
+
+#define MMWaveMaxQueueSize 120
+
 class SeeedmmWave {
  private:
   HardwareSerial* _serial;
   uint32_t _baud;
   uint32_t _wait_delay;
 
+  std::queue<std::vector<uint8_t>> byteQueue;
+
  protected:
-  uint16_t _id = 0;
+  size_t expectedFrameLength(const std::vector<uint8_t>& buffer);
   uint8_t calculateChecksum(const uint8_t* data, size_t len);
   bool validateChecksum(const uint8_t* data, size_t len,
                         uint8_t expected_checksum);
-  bool processFrame(const uint8_t* frame_bytes, size_t len, uint16_t data_type);
+  float extractFloat(const uint8_t* bytes) const;
+  uint32_t extractU32(const uint8_t* bytes) const;
+  void floatToBytes(float value, uint8_t* bytes);
+  void uint32ToBytes(uint32_t value, uint8_t* bytes);
 
+  bool processFrame(const uint8_t* frame_bytes, size_t len,
+                    uint16_t data_type = 0xFFFF);
   /**
    * @brief Handle different types of data frames.
    *
@@ -57,6 +92,10 @@ class SeeedmmWave {
   virtual bool handleType(uint16_t _type, const uint8_t* data,
                           size_t data_len) = 0;
 
+  std::vector<uint8_t> packetFrame(uint16_t type, const uint8_t* data = nullptr,
+                                   size_t len = 0);
+  bool sendFrame(const std::vector<uint8_t>& frame);
+
  public:
   SeeedmmWave() {}
   virtual ~SeeedmmWave() {
@@ -65,19 +104,37 @@ class SeeedmmWave {
       _serial = nullptr;
     }
   }
+
   void begin(HardwareSerial* serial, uint32_t baud = _UART_BAUD,
              uint32_t wait_delay = 1, int rst = -1);
   int available();
+  int read(void);
   int read(char* data, int length);
-  int write(const char* data, int length);
-  bool sendFrame(const uint16_t type, const uint8_t* data, size_t data_len);
+  size_t write(const uint8_t* buffer, size_t size);
+  size_t write(const char* buffer, size_t size);
 
-  bool fetch(uint16_t data_type = 0xFFFF, uint32_t timeout = 1);
+  /**
+   * @brief Send a frame of data.
+   *
+   * @attention This function constructs and sends a frame of data, including
+   * the frame header, data, and checksums.
+   *
+   * @param type The type of the frame.
+   * @param data The data to include in the frame. Defaults to nullptr.
+   * @param len The length of the data. Defaults to 0.
+   * @retval True if the frame is sent successfully
+   * @retval False otherwise.
+   */
+  bool update(uint32_t timeout = 100);
+  void fetch(uint32_t timeout = 1000);
+  bool fetchType(uint16_t data_type = 0xFFFF, uint32_t timeout = 1000);
+  bool send(uint16_t type, const uint8_t* data = nullptr, size_t data_len = 0);
 
-  float extractFloat(const uint8_t* bytes) const;
-  uint32_t extractU32(const uint8_t* bytes) const;
-  void floatToBytes(float value, uint8_t* bytes);
-  void uint32ToBytes(uint32_t value, uint8_t* bytes);
+  bool processQueuedFrames(uint16_t data_type = 0xFFFF,
+                           uint32_t timeout   = 1000);
+
 };
+
+void printHexBuff(const std::vector<uint8_t>& buffer);
 
 #endif  // SEEEDMMWAVE_H
